ef68da392496c9a61a123d058a0d94825319f719
TPalma-2023 <palmaalv@msu.edu>
2024-05-14 10:57:41 -0400

    Add crash test to app.js

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ mongoose
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger);
+
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(routes);
 
 app.use(errorLogger);
